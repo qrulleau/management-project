bcbb0c1c7aace74b7941e622d0e1712102ee53ea
qrulleau <quentin.rulleau@yahoo.com>
2024-03-14 11:46:00 +0100

    ADD: add service projet to get all project from db

--- src/Service/ProjectManager.php
+++ src/Service/ProjectManager.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\Project;
+use Doctrine\ORM\EntityManagerInterface;
+
 class ProjectManager
 {
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getAllProjects()
+    {
+        return $this->entityManager->getRepository(Project::class)->findAll();
+    }
 }
