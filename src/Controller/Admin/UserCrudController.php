5b1eff781c55093b18a451ff72c24a0f91d59123
qrulleau <quentin.rulleau@yahoo.com>
2024-03-14 19:48:10 +0100

    MIN: replace input to better ux experience

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -14,6 +15,22 @@ class UserCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            TextField::new('password'),
+            ChoiceField::new('roles', 'Rôles')
+                       ->setChoices([
+                           'ROLE_USER' => 'ROLE_USER',
+                           'ROLE_ADMIN' => 'ROLE_ADMIN',
+                       ])
+                       ->allowMultipleChoices(true)
+                       ->setFormTypeOption('choice_translation_domain', 'messages')
+        ];
     }
 
 }
