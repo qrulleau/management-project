2c760b03fb329a94bb707312d21601e12b77e06e
qrulleau <quentin.rulleau@yahoo.com>
2024-03-14 19:48:33 +0100

    ADD: set id method to user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,6 +32,13 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
         return $this->id;
     }
 
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
